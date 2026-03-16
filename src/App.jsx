0a0c88eae7f73d132e49723da37c3f76bcc300a6
Sean <seank5407@gmail.com>
2026-03-16 14:33:06 +0000

    Vercel Analytics added

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import Navbar from './components/Navbar'
 import Hero from './components/Hero'
 import About from './components/About'
@@ -16,6 +17,7 @@ function App() {
         <Contact />
       </main>
       <Footer />
+      <Analytics />
     </>
   )
 }
